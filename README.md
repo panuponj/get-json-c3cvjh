255cc48a79886e462a4417e57e5b85763b60f31b
panuponj <panuponj@gmail.com>
2021-04-12 07:01:38 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# get-json-c3cvjh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/get-json-c3cvjh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public jsonData;
+  private jsonUrl = 'https://graph.facebook.com/v10.0/507784162599885?fields=threads%7Bparticipants%2Cmessages.source(PARTICIPANTS).limit(1000)%7Btags%2Cmessage%7D%7D&access_token=EAAGFpZB6Ngn0BAMb8Oyu7ZBEW73TkH7fxrXDGA8JWkXuXQSPaQuuo33aM5DfZBWhIpZCL2h9B9Yk8ZAijdRa17ZAFNVLbEDzZBcT3RtnxkZBgRZAQnZARwY3khSReFNUIgvfl7AklI9RtMEbloAkyJhc3ZCwfcRoFPUaQQXoEXtO33EjKAkEAAPISdk2Ye5bLDgrcrvCVOKpMyUQYmxAimJPqd9'
+  public deviceArray: [];
+  public deviceArraytmp: [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getJson()
+  }
+
+  public getJson() {
+    return this.http.get(this.jsonUrl).subscribe((devicesArraytmp) => {
+      console.log('data ', devicesArraytmp);
+      
+      var nextpage ='';
+
+      this.devicesArray = devicesArraytmp.threads.data
+
+      this.jsonData = this.devicesArray;
+
+
+
+      
+
+
+      });
+
+    
+
+      for (let i = 0; i < this.jsonData.length; i++) {
+        // deviceArray.push(devices[i].lineId)
+        // this.deviceArray.push(this.deviceArray[i]);
+      }
+        console.log(this.deviceArray, " !!!!!!!!!!!!!!!!!");
+
+    });
+  }
+   
+  
+
+}
+
